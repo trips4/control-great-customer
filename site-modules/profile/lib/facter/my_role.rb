b1029ffd42b89180178fd463eacb15afae220a8e
trips4 <dennis.triplo@perforce.comm>
2026-03-04 16:47:25 -0600

    new fact

--- site-modules/profile/lib/facter/my_role.rb
+++ site-modules/profile/lib/facter/my_role.rb
@@ -1,12 +1,6 @@
 Facter.add(:my_role) do
   setcode do
-    trusted = Facter.value(:trusted)
-    certname = trusted.is_a?(Hash) ? trusted['certname'] : nil
-
-    if certname && certname.include?('almagt')
-      'my_win'
-    else
-      'hello_goodbye'
-    end
+    trusted = Facter.value(:trusted.certname)
+    trusted
   end
 end
