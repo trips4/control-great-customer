86dc321701b97604aa21c00763c09396f70c87d9
trips4 <dennis.triplo@perforce.comm>
2026-03-04 17:18:46 -0600

    new fact

--- site-modules/profile/lib/facter/my_role.rb
+++ site-modules/profile/lib/facter/my_role.rb
@@ -1,9 +1,10 @@
 Facter.add(:my_role) do
   setcode do
-    trusted = Facter.value('trusted')
-    certname = trusted.is_a?(Hash) ? trusted['certname'] : nil
-
-    name = certname && !certname.empty? ? certname : Facter.value('fqdn').to_s
-    name.include?('almagt') ? 'my_win' : 'default_role'
+    fqdn = Facter.value('fqdn').to_s
+    if fqdn.match?(/(oradev|oraprd|oranp|oramon|oradba|almagt)/)
+      'db_server'
+    else
+      'unknown_role'
+    end
   end
 end
