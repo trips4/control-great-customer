7d22b7ee611effa16b815e6d210a1f7038a208dd
trips4 <dennis.triplo@perforce.comm>
2026-03-04 17:53:55 -0600

    new fact

--- site-modules/profile/lib/facter/saif_role.rb
+++ site-modules/profile/lib/facter/saif_role.rb
@@ -1,8 +1,10 @@
-Facter.add(:my_role) do
+Facter.add(:saif_role) do
   setcode do
-    fqdn = Facter.value('fqdn').to_s
+    fqdn = Facter.value('fqdn').to_s.downcase
     if fqdn.match?(/(oradev|oraprd|oranp|oramon|oradba|almagt)/)
       'db_server'
+    elsif fqdn.match?(/(guiddev|guidprd)/)
+      'guidewire'
     else
       'unknown_role'
     end
