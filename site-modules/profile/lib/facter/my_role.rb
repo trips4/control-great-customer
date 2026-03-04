4e4a61463f2b0f9e6975f1d03be2c154ba1d5871
trips4 <dennis.triplo@perforce.comm>
2026-03-04 16:06:28 -0600

    new fact

---
+++ site-modules/profile/lib/facter/my_role.rb
@@ -0,0 +1,22 @@
+
+# modules/site_facts/lib/facter/role.rb
+
+Facter.add(:my_role) do
+  setcode do
+    # Get the certname Puppet uses for this node
+    certname = Facter.value(:clientcert)
+
+    # Defensive: handle nil
+    next nil if certname.nil? || certname.empty?
+
+    # Normalize to lowercase for case-insensitive match
+    cert = certname.downcase
+
+    if cert.include?('ora') || cert.include?('oradv')
+      'db_server'
+    else
+      # You can choose something else here, like 'agent_only' or nil
+      'unknown'
+    end
+  end
+end
