a90461c0c57eb4404c21edd368fbb8302c9bb0a2
trips4 <dennis.triplo@perforce.comm>
2026-03-04 16:55:04 -0600

    new fact

--- site-modules/profile/lib/facter/my_role.rb
+++ site-modules/profile/lib/facter/my_role.rb
@@ -2,6 +2,6 @@ Facter.add(:my_role) do
   setcode do
     trusted = Facter.value('trusted')
     certname = trusted.is_a?(Hash) ? trusted['certname'] : ''
-    certname.include?('winagt') ? 'my_win' : 'default_role'
+    certname.include?('almagt') ? 'my_win' : 'default_role'
   end
 end
